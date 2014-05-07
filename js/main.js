3d406b24b3374522399eaeda53ee5cec75aab506
Ian Cottam <ian.cottam@hogarthww.com>
2014-05-07 18:00:37 +0100

    1800.07.05.14

--- js/main.js
+++ js/main.js
@@ -1,37 +1,52 @@
+var LP = (function(){
+		// make all boxes equal height
 
-		equalheight = function(container){
+	return{
 
-					var currentTallest = 0,
-					     currentRowStart = 0,
-					     rowDivs = new Array(),
-					     $el,
-					     topPosition = 0;
-					 $(container).each(function() {
+		equalheight: function(container){
 
-					   $el = $(this);
-					   $($el).height('auto')
-					   topPosition = $el.position().top;
+			var currentTallest = 0,
+			    currentRowStart = 0,
+			    rowDivs = new Array(),
+			    $el,
+			    topPosition = 0;
 
-					   if (currentRowStart != topPosition) {
-					     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-					       rowDivs[currentDiv].height(currentTallest);
-					     }
-					     rowDivs.length = 0; // empty the array
-					     currentRowStart = topPosition;
-					     currentTallest = $el.height();
-					     rowDivs.push($el);
-					   } else {
-					     rowDivs.push($el);
-					     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
-					  }
-					   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-					     rowDivs[currentDiv].height(currentTallest);
-					   }
-					 });
-					}
-		
-		$(window).resize(function(){
-		  equalheight(equalClass);
-		});	
+			$(container).each(function() {
+			    $el = $(this);
+			    $($el).height('auto')
+			    topPosition = $el.position().top;
+			    if (currentRowStart != topPosition) {
+			    	for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+			    	rowDivs[currentDiv].height(currentTallest);
+			    }
+			    rowDivs.length = 0; // empty the array
+			    currentRowStart = topPosition;
+			    currentTallest = $el.height();
+			    rowDivs.push($el);
+			    } else {
+			    	rowDivs.push($el);
+			    	currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+				}
+			    for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+			    	rowDivs[currentDiv].height(currentTallest);
+				}
+			});
 
-		var equalClass = '.row-one, .row-two, .row-three, .row-four'
+		},
+
+		positionCta: function(){
+			// if... add class cta-wrapper-bottom
+		}
+
+	}
+
+})();
+
+
+var equalClass = '.pod1, .pod2, .pod3 , .cta-box';
+
+$(window).resize(function(){
+	LP.equalheight(equalClass);
+});
+
+LP.equalheight(equalClass);
